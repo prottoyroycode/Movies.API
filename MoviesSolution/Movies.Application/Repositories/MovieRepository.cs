34ffae5ee0fc617e9136222c2cd3b7960b501b58
prottoyroycode <prottoyroycode@gmail.com>
2024-09-02 18:01:49 +0600

    adding movie repository

---
+++ MoviesSolution/Movies.Application/Repositories/MovieRepository.cs
@@ -0,0 +1,37 @@
+﻿using Movies.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Application.Repositories
+{
+    internal class MovieRepository : IMovieRepository
+    {
+        public Task<bool> CreateAsync(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Movie>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MoviesSolution/Movies.Application/obj/Debug/net8.0/Movies.Application.AssemblyInfo.cs
+++ MoviesSolution/Movies.Application/obj/Debug/net8.0/Movies.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c89d9bc4e2f59674f151f0ff77f105fe7542d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9113bb6cc94c7dfed6463a43f87c3a5099875cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
